dc520409c6c7fd805657bd28a3a813792c8715c5
mdhamid23 <hamid@deepchainlabs.com>
2026-01-04 08:33:23 +0600

    Ajax and json added

--- Final/Section_U/Model/DatabaseConnection.php
+++ Final/Section_U/Model/DatabaseConnection.php
@@ -24,7 +24,23 @@ class DatabaseConnection{
         $result = $connection->query($sql);
         return $result;
     }
+    
 
+    function checkExistingUser($connection, $tableName, $email){
+        $sql = "SELECT * FROM ".$tableName." WHERE email='".$email."'";
+        $result = $connection->query($sql);
+        return $result;
+    }
+    
+    function InsertData($connection,$table,$email, $password)
+    {
+        $sql = "INSERT INTO users (email,password) VALUES(?,?)";
+        $stmt=$connection->prepare($sql); 
+        $stmt->bind_param("ss",$email,$password);
+        $result = $stmt->execute();
+        return $result;
+    
+    }
 
     function closeConnection($connection){
         $connection->close();
--- Final/Section_U/View/signup.php
+++ Final/Section_U/View/signup.php
@@ -22,15 +22,17 @@ unset($_SESSION["loginErr"]);
 ?>
 
 <html>
-
+<head>
+    <script src="../Controller/JS/checkEmail.js"></script>
+</head>
 <body>
 <!-- <pre><?php echo $previousValues["email"];?></php></pre> -->
-<form method="post" onsubmit="" action="..\Controller\handleSignupValidation.php" enctype="multipart/form-data">
+<form method="post" onsubmit="" action="../Controller/handleSignupValidation.php" enctype="multipart/form-data">
     <table>
         <tr>
             <td>Email</td>
-            <td><input type="text" name="email"  placeholder="Enter your email" value="<?php echo $previousValues["email"] ?? "" ?>"/> </td>
-            <td><?php echo $emailErr; ?></td>
+            <td><input type="text" name="email" id="email" onkeyup="findExistingEmail()" placeholder="Enter your email" value="<?php echo $previousValues["email"] ?? "" ?>" /> </td>
+            <td><p id="ajaxResponse"></p></td>
         </tr>
         <tr>
             <td>Password</td>
